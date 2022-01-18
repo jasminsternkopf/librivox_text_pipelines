62ef682ec93a84f88df0a4251880906e173c800b
Jasmin Sternkopf <jasmin_sternkopf@web.de>
2022-01-18 14:08:28 +0100

    deletes old attempts with unidecode

---
+++ tests/test_unidecode_with_ignoreset.py
@@ -0,0 +1,17 @@
+from text_pipeline.adjustments.unidecode_with_ignoreset import \
+    unidecode_with_ignore_set
+
+
+def test_unidecode_with_ignore_set__no_ignore_set():
+  text = "Κνωσός"
+  res = unidecode_with_ignore_set(text)
+
+  assert res == "Knosos"
+
+
+def test_unidecode_with_ignore_set():
+  text = "ÄbCdéèfGüÜ"
+  ignore_set = {"ä", "ü", "é"}
+  res = unidecode_with_ignore_set(text, ignore_set)
+
+  assert res == "AbCdéefGüU"
