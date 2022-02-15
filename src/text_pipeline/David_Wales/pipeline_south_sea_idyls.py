5b3e9994378d44367731ab94b2d9e68fcd5caf6e
Jasmin Sternkopf <jasmin_sternkopf@web.de>
2022-02-15 15:06:00 +0100

    south sea idyls small adj., now really finished

--- src/text_pipeline/David_Wales/pipeline_south_sea_idyls.py
+++ src/text_pipeline/David_Wales/pipeline_south_sea_idyls.py
@@ -4,10 +4,10 @@ from english_text_normalization import (
     normalize_today_and_tomorrow, remove_dot_after_single_capital_letters,
     remove_double_hyphen_before_or_after_colon, remove_equal_sign,
     remove_everything_in_square_brackets, remove_four_hyphens,
-    remove_linebreaks, remove_quotation_marks_when_used_as_itemization,
-    remove_repeated_spaces, remove_stars, remove_tilde,
-    remove_underscore_characters, replace_etc_with_et_cetera,
-    write_out_month_abbreviations)
+    remove_linebreaks, remove_plus,
+    remove_quotation_marks_when_used_as_itemization, remove_repeated_spaces,
+    remove_stars, remove_tilde, remove_underscore_characters,
+    replace_etc_with_et_cetera, write_out_month_abbreviations)
 from LJ_and_DW_adjustments import (L_into_Lew,
                                    footnote_normalization_in_south_sea_idyls,
                                    four_hyphens_followed_by_dot_into_blank,
@@ -35,6 +35,7 @@ def normalize_south_sea_idyls(text: str) -> str:
   text = remove_underscore_characters(text)
   text = remove_equal_sign(text)
   text = remove_tilde(text)
+  text = remove_plus(text)
   text = four_hyphens_followed_by_dot_into_blank(text)
   text = L_into_Lew(text)
   text = remove_four_hyphens(text)
